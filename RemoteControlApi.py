01e82f77a87e38a66084bb8d2167f575e2acd770
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-09-10 12:13:23 +0200

    added more functions to the RemoteControlApi, changed Unittests to use fastapi.testclient.TestClient instead of subprocess.

--- RemoteControlApi.py
+++ RemoteControlApi.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Body
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from typing import Any, List
 import subprocess  # skipcq: BAN-B404
 import shlex
 import json
 
 app = FastAPI()
-ERROR_MESSAGE_RESOURCE_NOT_FOUND = b'"Resource \\\\"%s\\\\" could not be found."'
+ERR_RESOURCE_NOT_FOUND = b'"Resource \\\\"%s\\\\" could not be found."'
+
+
+class ConfigProperty(BaseModel):
+    name: str
+    value: Any
 
 
 @app.get("/")
@@ -14,6 +21,9 @@ def get_current_config():
     res = subprocess.run(
         ['sudo', 'python3', 'AMinerRemoteControl', '--Exec', 'print_current_config(analysis_context)', '--StringResponse'],
         capture_output=True)
+    # lines = res.stdout.split(b':', 1)[1].strip(b' ').split(b'\n')
+    # for i, line in enumerate(lines[:300]):
+    #     print(i+1, line)
     return json.loads((b'{' + res.stdout.split(b':', 1)[1].strip(b' ') + b'}'))
 
 
@@ -22,11 +32,8 @@ def get_config_property(config_property: str):
     # skipcq: BAN-B603, BAN-B607, PYL-W1510
     res = subprocess.run(['sudo', 'python3', 'AMinerRemoteControl', '--Exec', 'print_config_property(analysis_context,"%s")'
                           % shlex.quote(config_property)], capture_output=True)
-    print(res.stdout)
     val = res.stdout.split(b"'")[1]
-    if val == ERROR_MESSAGE_RESOURCE_NOT_FOUND % config_property.encode('utf-8'):
-        print(val)
-        print(val.decode())
+    if val == ERR_RESOURCE_NOT_FOUND % config_property.encode('utf-8'):
         return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'ErrorMessage': val.decode().replace('\\', '').strip('"')})
     val = val.split(b':', 1)[1].strip(b' ')
     if val.startswith(b'[') and val.endswith(b']'):
@@ -40,3 +47,16 @@ def get_config_property(config_property: str):
             except:  # skipcq: FLK-E722
                 pass
     return {config_property: val}
+
+
+@app.put("/config_property/")
+def put_config_property(config_property: ConfigProperty):
+    # first check if the property exists - return the status code.
+    print(config_property)
+    return JSONResponse(status_code=status.HTTP_201_CREATED)
+
+
+@app.post("/config_property/")
+def post_config_property(config_property: str):
+    # first check if the property exists - return the status code.
+    pass
--- unit/RemoteControlApiTest.py
+++ unit/RemoteControlApiTest.py
@@ -1,6 +1,6 @@
 import unittest
-import subprocess  # skipcq: BAN-B404
-from RemoteControlApi import ERROR_MESSAGE_RESOURCE_NOT_FOUND
+from RemoteControlApi import ERR_RESOURCE_NOT_FOUND, app
+from fastapi.testclient import TestClient
 
 
 class RemoteControlApiTest(unittest.TestCase):
@@ -8,33 +8,30 @@ class RemoteControlApiTest(unittest.TestCase):
     and must be done beforehand."""
     cmd = ['curl', '-i', '-H', "Accept: application/json", '-H', "Content-Type: application/json", '-X', 'GET']
     config_property_addr = 'http://127.0.0.1:8000/config_property/%s'
+    client = TestClient(app)
 
     def test1get_config_property(self):
         property_name = 'MailAlerting.MaxEventsPerMessage'
-        # skipcq: BAN-B603, PYL-W1510
-        res = subprocess.run(self.cmd + [self.config_property_addr % property_name], capture_output=True)
-        self.assertIn(b'200 OK', res.stdout)
-        self.assertIn(b'content-type: application/json', res.stdout)
-        self.assertIn(b'{"%s":1000}' % property_name.encode('utf-8'), res.stdout)
+        response = self.client.get('config_property/%s' % property_name)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers['content-type'], 'application/json')
+        self.assertEqual(response.content, b'{"%s":1000}' % property_name.encode('utf-8'))
 
         property_name = 'AMinerUser'
-        # skipcq: BAN-B603, PYL-W1510
-        res = subprocess.run(self.cmd + [self.config_property_addr % property_name], capture_output=True)
-        self.assertIn(b'200 OK', res.stdout)
-        self.assertIn(b'content-type: application/json', res.stdout)
-        self.assertIn(b'{"%s":"aminer"}' % property_name.encode('utf-8'), res.stdout)
+        response = self.client.get('config_property/%s' % property_name)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers['content-type'], 'application/json')
+        self.assertEqual(response.content, b'{"%s":"aminer"}' % property_name.encode('utf-8'))
 
         property_name = 'LogResourceList'
-        # skipcq: BAN-B603, PYL-W1510
-        res = subprocess.run(self.cmd + [self.config_property_addr % property_name], capture_output=True)
-        self.assertIn(b'200 OK', res.stdout)
-        self.assertIn(b'content-type: application/json', res.stdout)
-        self.assertIn(b'{"%s":["file:///tmp/syslog"]}' % property_name.encode('utf-8'), res.stdout)
+        response = self.client.get('config_property/%s' % property_name)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers['content-type'], 'application/json')
+        self.assertEqual(response.content, b'{"%s":["file:///tmp/syslog"]}' % property_name.encode('utf-8'))
 
         property_name = 'NoneExistentConfigProperty'
-        # skipcq: BAN-B603, PYL-W1510
-        res = subprocess.run(self.cmd + [self.config_property_addr % property_name], capture_output=True)
-        self.assertIn(b'404 Not Found', res.stdout)
-        self.assertIn(b'content-type: application/json', res.stdout)
-        self.assertIn(b'{"ErrorMessage":%s}' % ERROR_MESSAGE_RESOURCE_NOT_FOUND.replace(b'\\\\', b'\\') % property_name.encode('utf-8'),
-                      res.stdout)
+        response = self.client.get('config_property/%s' % property_name)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.headers['content-type'], 'application/json')
+        self.assertEqual(response.content, b'{"ErrorMessage":%s}' % ERR_RESOURCE_NOT_FOUND.replace(
+            b'\\\\', b'\\') % property_name.encode('utf-8'))
