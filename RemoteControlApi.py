4b3690cd1b9ce5bc03783fd4528ab8d413e0e59a
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-09-10 20:21:28 +0200

    added the PUT method for config_property (not finished).

--- RemoteControlApi.py
+++ RemoteControlApi.py
@@ -1,4 +1,19 @@
+"""This module contains methods to access the AMinerRemoteControl by the REST-API.
+The implementation follows the RFC-2616 standard.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from fastapi import FastAPI, status
+from fastapi.testclient import TestClient
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from typing import Any, List
@@ -7,11 +22,12 @@ import shlex
 import json
 
 app = FastAPI()
+client = TestClient(app)
 ERR_RESOURCE_NOT_FOUND = b'"Resource \\\\"%s\\\\" could not be found."'
+CONFIG_PROPERTY_PATH = "/config_property/{config_property}"
 
 
 class ConfigProperty(BaseModel):
-    name: str
     value: Any
 
 
@@ -27,7 +43,7 @@ def get_current_config():
     return json.loads((b'{' + res.stdout.split(b':', 1)[1].strip(b' ') + b'}'))
 
 
-@app.get("/config_property/{config_property}")
+@app.get(CONFIG_PROPERTY_PATH)
 def get_config_property(config_property: str):
     # skipcq: BAN-B603, BAN-B607, PYL-W1510
     res = subprocess.run(['sudo', 'python3', 'AMinerRemoteControl', '--Exec', 'print_config_property(analysis_context,"%s")'
@@ -49,14 +65,19 @@ def get_config_property(config_property: str):
     return {config_property: val}
 
 
-@app.put("/config_property/")
-def put_config_property(config_property: ConfigProperty):
+@app.put(CONFIG_PROPERTY_PATH)
+def put_config_property(config_property: str, item: ConfigProperty):
     # first check if the property exists - return the status code.
-    print(config_property)
-    return JSONResponse(status_code=status.HTTP_201_CREATED)
+    response = client.get("%s%s" % (CONFIG_PROPERTY_PATH.split("{")[0], config_property))
+    if response.status_code == 200:
+        return JSONResponse(status_code=status.HTTP_201_CREATED)
+    elif response.status_code == 404:
+        return JSONResponse(status_code=status.HTTP_200_OK)
+    else:
+        return JSONResponse(status_code=response.status_code)
 
 
-@app.post("/config_property/")
-def post_config_property(config_property: str):
+@app.post(CONFIG_PROPERTY_PATH.split("{")[0])
+def post_config_property(config_property: ConfigProperty):
     # first check if the property exists - return the status code.
     pass
