db72219d6dd84a4251cfa497b340a03a3e5951c5
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-09-15 17:27:31 +0200

    fixed Travis errors.

--- runUnittests.sh
+++ runUnittests.sh
@@ -1,3 +1,6 @@
+mkdir /tmp/lib
+mkdir /tmp/lib/aminer
+sudo chown -R aminer:aminer /tmp/lib
 cp logdata-anomaly-miner/aecid-testsuite/demo/AMinerRemoteControl/demo-config.py /tmp
 sudo AMiner --Foreground --Config /tmp/demo-config.py
 sudo python3 -m unittest discover -s unit -p '*Test.py' > /dev/null
@@ -5,4 +8,5 @@ exit_code=$?
 sudo pkill AMiner
 sudo rm /tmp/AMinerRemoteLog.txt
 rm /tmp/demo-config.py
+sudo rm -r /tmp/lib
 exit $exit_code
