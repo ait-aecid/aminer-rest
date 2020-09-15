0873bce8164a41afda6d107e1f7ccf0a8282b897
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-09-15 17:23:16 +0200

    added cp of config in runUnittests.sh.

--- runUnittests.sh
+++ runUnittests.sh
@@ -1,6 +1,8 @@
+cp logdata-anomaly-miner/aecid-testsuite/demo/AMinerRemoteControl/demo-config.py /tmp
 sudo AMiner --Foreground --Config /tmp/demo-config.py
 sudo python3 -m unittest discover -s unit -p '*Test.py' > /dev/null
 exit_code=$?
 sudo pkill AMiner
 sudo rm /tmp/AMinerRemoteLog.txt
+rm /tmp/demo-config.py
 exit $exit_code
