b3cfc0d8ab698e6812ddc7c0c62089b2cd05efb4
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-10-27 08:47:54 +0100

    removed unnessecary start of aminer for the aminer-rest unittests.

--- runUnittests.sh
+++ runUnittests.sh
@@ -1,13 +1,3 @@
-mkdir /tmp/lib
-mkdir /tmp/lib/aminer
-sudo chown -R aminer:aminer /tmp/lib
-cp logdata-anomaly-miner/aecid-testsuite/demo/AMinerRemoteControl/demo-config.py /tmp
-sudo AMiner --Foreground --Config /tmp/demo-config.py &
-sleep 2
-sudo python3 -m unittest discover -s unit -p '*Test.py' #> /dev/null
+sudo python3 -m unittest discover -s unit -p '*Test.py' > /dev/null
 exit_code=$?
-sudo pkill AMiner
-sudo rm /tmp/AMinerRemoteLog.txt
-rm /tmp/demo-config.py
-sudo rm -r /tmp/lib
 exit $exit_code
