f86db5a3a370209e3f3beb36d0bda4daae353b50
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-10-27 11:14:31 +0100

    removed AMinerRemoteControl from Travis.

--- runCoverageTests.sh
+++ runCoverageTests.sh
@@ -2,7 +2,7 @@ mkdir /tmp/lib
 mkdir /tmp/lib/aminer
 sudo chown -R aminer:aminer /tmp/lib
 cp logdata-anomaly-miner/aecid-testsuite/demo/AMinerRemoteControl/demo-config.py /tmp
-sudo AMiner --Foreground --Config /tmp/demo-config.py &
+sudo aminer --Config /tmp/demo-config.py &
 sleep 2
 sudo coverage run --source=./ -m unittest discover -s unit -p '*Test.py' > /dev/null
 exit_code1=$?
@@ -21,7 +21,7 @@ rm /tmp/report
 test -e /var/mail/mail && sudo rm -f /var/mail/mail
 sudo rm /tmp/AMinerRemoteLog.txt
 rm /tmp/demo-config.py
-sudo pkill AMiner
+sudo pkill aminer
 sudo rm -r /tmp/lib
 if [[ "$exit_code1" -ne 0 || "$exit_code2" -ne 0 ]]; then
 	exit 1
